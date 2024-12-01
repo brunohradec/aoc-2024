ddf674a3668290914525fb771cf9b2b897dca4c1
Bruno Hradec <bhradec@gmail.com>
2024-12-01 15:22:57 +0100

    Add day 1 part 2 solution

---
+++ src/main/java/me/bhradec/aoc2024/day1/part2/Day1Part2Solution.java
@@ -0,0 +1,86 @@
+package me.bhradec.aoc2024.day1.part2;
+
+import org.apache.commons.cli.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Day1Part2Solution {
+    private static final Logger log = LoggerFactory.getLogger(Day1Part2Solution.class);
+
+    private List<Integer> leftList = new ArrayList<>();
+    private Map<Integer, Integer> rightListfrequency = new HashMap<>();
+
+    private void parseInput(String path) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            String line = reader.readLine();
+
+            while (line != null) {
+                Integer leftNum = Integer.valueOf(line.split("\\s+")[0]);
+                Integer rightNum = Integer.valueOf(line.split("\\s+")[1]);
+
+                leftList.add(leftNum);
+
+                if (rightListfrequency.get(rightNum) != null) {
+                    rightListfrequency.put(rightNum, rightListfrequency.get(rightNum) + 1);
+                } else {
+                    rightListfrequency.put(rightNum, 1);
+                }
+
+                line = reader.readLine();
+            }
+        }
+    }
+
+    private long getSimilarityScore() {
+        long similarityScore = 0;
+
+        for (int leftNum : leftList) {
+            similarityScore += (long) leftNum * rightListfrequency.getOrDefault(leftNum, 0);
+        }
+
+        return similarityScore;
+    }
+
+    public static void main(String[] args) {
+        Options options = new Options();
+
+        Option inputPathOption = Option.builder("i")
+                .longOpt("input")
+                .argName("INPUT_PATH")
+                .desc("Path to the input file")
+                .hasArg()
+                .required(true)
+                .build();
+
+        options.addOption(inputPathOption);
+
+        CommandLineParser commandLineParser = new DefaultParser();
+
+        CommandLine commandLine;
+
+        try {
+            commandLine = commandLineParser.parse(options, args);
+        } catch (ParseException exception) {
+            log.error("Error parsing arguments", exception);
+            return;
+        }
+
+        String inputPath = commandLine.getOptionValue(inputPathOption);
+
+        Day1Part2Solution day1Part2Solution = new Day1Part2Solution();
+
+        try {
+            day1Part2Solution.parseInput(inputPath);
+        } catch (IOException exception) {
+            log.error("Could not read input file", exception);
+            return;
+        }
+
+        log.info("Result: {}", day1Part2Solution.getSimilarityScore());
+    }
+}
